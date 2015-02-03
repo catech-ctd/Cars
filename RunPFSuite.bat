8730ec6fb122339f29400e7ca40a56ca0227285e
catech-ctd <marta.benach@ca.com>
2015-02-03 16:19:12 -0500

    RunPFSuite.bat

---
+++ RunPFSuite.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\CA\DevTest\bin\testrunner" -s "C:\Program Files\CA\DevTest\cars-vs\Suites\carDetails_dispatch.ste" -a -config "C:\Program Files\CA\DevTest\cars-vs\Configs\project.config"
